63db6040332f9d27b6b709b4badf8dc598cddd3d
Prashant Kumar <43726309+Prashant26th@users.noreply.github.com>
2025-09-12 14:56:47 +0530

    Update HomeForm.tsx

--- src/components/HomeForm.tsx
+++ src/components/HomeForm.tsx
@@ -101,7 +101,7 @@ const HomeForm: React.FC = () => {
     console.log('Submitting form with data:', { date, truckNumber, transporter, checklist, photos });
 
     try {
-      const response = await fetch('https://163209aa37f2.ngrok-free.app/validation', {
+      const response = await fetch('https://581a09000db5.ngrok-free.app/validation', {
         method: 'POST',
         body: formData,
       });
